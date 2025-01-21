4413dcb6904f1efe6feeadb7e2bd17d7d4c1a7aa
Sorin VIeriu <sorinvieriu@gmail.com>
2025-01-21 21:58:01 +0200

    feat: added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import * as dotenv from "dotenv";
 
+import tailwind from '@astrojs/tailwind';
+
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), tailwind()],
 });
